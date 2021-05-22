91b80f58ee1bbb596afce60e33ea983888d35c39
Samuel Costa <samuel.costa@ccc.ufcg.edu.br>
2021-05-22 18:13:59 -0300

    arrumando os readme

---
+++ Prolog/README.md
@@ -0,0 +1,7 @@
+<h1>Tutorial de como rodar a aplicação</h1>
+<p>Após fazer o clone da aplicação e entrar na pasta correspondente, escreva no terminal os seguinte comandos: </p>
+
+```pl
+swipl
+main.
+```
--- Prolog/bonusSalarial.pl
+++ Prolog/bonusSalarial.pl
@@ -1,25 +1,31 @@
 :-module(bonusSalarial, [listaBonusSalarial/0, cadastraBonusSalarial/0,
 	excluiBonusSalarial/0, editaBonusSalarial/0]).
 
-ListaDeBonusSalarial = [].
+:- dynamic bonus/1.
+bonus(0).
+
 
 cadastraBonusSalarial:- 
 	write("Digite o valor do seu bônus salarial em reais:"), nl,
 	read(Bonus),
-	append(ListaDeBonusSalarial, [Bonus], List),
-	ListaDeBonusSalarial = List.
+	bonus(X),
+	BonusNovo is X + Bonus,
+	asserta(bonus(BonusNovo)),
 	write("Bônus Salarial cadastrado com sucesso!!"), nl.
 
 listaBonusSalarial:- 
-	Res = "",
-	percorre(ListaDeBonusSalarial).
+	bonus(X),
+	(X == 0) -> write("Ainda não foi cadastrado nenhum bônus salarial."), nl, write("O que deseja fazer?"), nl;
+	write("Seu bônus salarial total é R$"),
+	bonus(X),
+	write(X), nl.
 
 excluiBonusSalarial:- 
-	write("OI").
+	bonus(X),
+	(X == 0) -> write("Ainda não foi cadastrado nenhum bônus salarial."), nl, write("O que deseja fazer?"), nl;
+	retractall(bonus(_)),
+	asserta(bonus(0)),
+	write("Bônus salarial zerado com sucesso!!"), nl.
 
 editaBonusSalarial:- 
 	write("OI").
-
-remove(X, [X|C], C).
-remove(X [Y|C], [Y|D]):-
-	remove(X, C, D).

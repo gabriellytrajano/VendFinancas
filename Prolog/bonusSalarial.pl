87a5f19b266051885b30d4f6363a6450c6071fe7
Samuel Costa <samuel.costa@ccc.ufcg.edu.br>
2021-05-22 17:50:01 -0300

    comecei a implementar bonus salarial

--- Prolog/bonusSalarial.pl
+++ Prolog/bonusSalarial.pl
@@ -1,15 +1,25 @@
 :-module(bonusSalarial, [listaBonusSalarial/0, cadastraBonusSalarial/0,
 	excluiBonusSalarial/0, editaBonusSalarial/0]).
 
-listaBonusSalarial:- 
-	write("OI").
-
+ListaDeBonusSalarial = [].
 
 cadastraBonusSalarial:- 
-	write("OI").
+	write("Digite o valor do seu bônus salarial em reais:"), nl,
+	read(Bonus),
+	append(ListaDeBonusSalarial, [Bonus], List),
+	ListaDeBonusSalarial = List.
+	write("Bônus Salarial cadastrado com sucesso!!"), nl.
+
+listaBonusSalarial:- 
+	Res = "",
+	percorre(ListaDeBonusSalarial).
 
 excluiBonusSalarial:- 
 	write("OI").
 
 editaBonusSalarial:- 
 	write("OI").
+
+remove(X, [X|C], C).
+remove(X [Y|C], [Y|D]):-
+	remove(X, C, D).

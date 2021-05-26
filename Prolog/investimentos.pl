3e0ef5c57edb477cc991684d4fe986d0ae6d0266
Mariana Victoria Coimbra <42918634+marianacoimbra@users.noreply.github.com>
2021-05-25 22:28:59 -0300

    Update formulas

--- Prolog/investimentos.pl
+++ Prolog/investimentos.pl
@@ -7,19 +7,25 @@ jurosSimples:-
     read(Taxa),
     write("Digite o tempo em meses a ser calculado: "),nl,
     read(Tempo),
-    Res is ((Capital * (Taxa/100))/Tempo),
+    Res is ((Capital * (Taxa/100))*Tempo),
+    Mon is (Capital + Res),
     tty_clear,
+    write("Nessa modalidade o seu dinheiro rendeu: R$"),
+    write(Res),nl,
     write("O montante total do seu investimento é de: R$"),
-    write(Res),nl.
+    write(Mon), nl.
 
 jurosCompostos:-
-	write("Digite o capital a ser calculado: "), nl,
+    write("Digite o capital a ser calculado: "), nl,
     read(Capital),
     write("Digite a taxa a ser calculada: "), nl,
     read(Taxa),
     write("Digite o tempo em meses a ser calculado: "),nl,
     read(Tempo),
     Res is (Capital * ((1 + (Taxa/100))^Tempo)),
+    Mon is (Capital + Res),
     tty_clear,
+    write("Nessa modalidade o seu dinheiro rendeu: R$"),
+    write(Res),nl,
     write("O montante total do seu investimento é de: R$"),
-    write(Res),nl.
+    write(Mon), nl.
